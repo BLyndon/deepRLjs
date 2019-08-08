51c42f604bead124545aa21a4a25e3dc8d7f9484
Benjamin Heil <bheil@mail.de>
2019-08-08 14:12:06 +0200

    backwardprop sigmoid added

---
+++ js/bwSigmoid.js
@@ -0,0 +1,12 @@
+import { dimTest } from 'relu'
+import { dotMultiply, size, subtract } from 'mathjs'
+import sigmoid from 'sigmoid'
+
+export default function bwSigmoid(dA, cache) {
+    let Z = cache;
+    let sigprime = dotMultiply(sigmoid(Z), subtract(1, sigmoid(Z)));
+    let dZ = dotMultiply(dA, sigprime);
+
+    console.assert(dimTest(dZ, Z), "Error bwSigmoid: shape dZ: " + size(dZ) + ", shape Z: " + size(Z) + ".");
+    return dZ;
+};
