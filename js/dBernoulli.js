6733e53d84836e0b6bb886f29fc9c2aee6adf7d6
Benjamin Heil <bheil@mail.de>
2019-08-08 20:47:31 +0200

    error msg. chagned

--- js/dBernoulli.js
+++ js/dBernoulli.js
@@ -1,17 +1,23 @@
-import { numTest, dimTest } from 'tests'
+import { isNum, dimTest } from 'tests'
 import { pow, transpose, concat } from 'mathjs'
 
-
-export default function dlogBer(p, a) {
-    if (numTest(p) && numTest(a)) {
+/**
+ * Derivative of the log-probability for a pair
+ * (p, a), as well as for vector of tuples.
+ * @param   {matrix}
+ * @param   {matrix}
+ * @return  {matrix}
+ */
+export default function dlogBernoulli(p, a) {
+    if (isNum(p) && isNum(a)) {
         console.assert(p > 0 && p < 1, "p out of bounds!");
-        console.assert(a == 0 || a == 1, "Wrong action!");
+        console.assert(a == 0 || a == 1, "Action-value wrong!");
         return (a - p) / (p - pow(p, 2));
     }
     else {
-        console.assert(dimTest(p, a), "Wrong dimensions!");
+        console.assert(dimTest(p, a), "Vectors have different lengths!");
         var pa = transpose(concat(p, a, 0));
-        let res = pa.map(items => dlogBer(items[0], items[1]));
+        let res = pa.map(items => dlogBernoulli(items[0], items[1]));
         return res;
     }
 };
