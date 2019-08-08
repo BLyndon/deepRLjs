4ac2e64c43aa365329e7ebe9d0d7b9ce42164f46
Benjamin Heil <bheil@mail.de>
2019-08-08 13:55:51 +0200

    step function added

---
+++ js/heaviside.js
@@ -0,0 +1,10 @@
+import { numbTest } from tests.js
+
+export default function heaviside(x) {
+    if (numbTest(x)) {
+        return +(x >= 0);
+    }
+    else {
+        return x.map(item => this.heaviside(item));
+    }
+};
