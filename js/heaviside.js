553549e7eb572903a1a301c6c7e0ffb8be63eeb4
Benjamin Heil <bheil@mail.de>
2019-08-08 16:54:14 +0200

    doc

--- js/heaviside.js
+++ js/heaviside.js
@@ -1,5 +1,12 @@
 import { numbTest } from tests.js
 
+/**
+ * Heaviside step func. applied element-wise:  
+ *      x -> 0 if x < 0 
+ *      x -> 1 else  
+ * @param   {matrix}
+ * @return  {matrix}
+ */
 export default function heaviside(x) {
     if (numbTest(x)) {
         return +(x >= 0);
