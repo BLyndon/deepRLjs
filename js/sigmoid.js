77d56703491028d7544b3adbf2e909ca792baa10
Benjamin Heil <bheil@mail.de>
2019-08-08 13:44:44 +0200

    sigmoid added

---
+++ js/sigmoid.js
@@ -0,0 +1,11 @@
+import { numbTest } from tests.js
+
+export default function sigmoid(x) {
+    if (numbTest(x)) {
+        return 1 / (1 + Math.exp(-x));
+    }
+    else {
+        let res = x.map(item => this.sigmoid(item));
+        return res;
+    }
+};
