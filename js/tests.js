dbbb651e08fcea1f14bc8e23247c01f73e893921
Benjamin Heil <bheil@mail.de>
2019-08-08 20:41:42 +0200

    doc added, changed to more meaningful func name

--- js/tests.js
+++ js/tests.js
@@ -1,11 +1,23 @@
 import { size } from 'mathjs'
 
-function numbTest(a) {
+/**
+ * Check type of the input.
+ * @param   {matrix}
+ * @return  {boolean}
+ */
+function isNum(a) {
     return typeof (a) == 'number' ? true : false
 };
 
+
+/**
+ * Check if dimensions of input matrices match.
+ * @param   {matrix}
+ * @param   {matrix}
+ * @return  {boolean}
+ */
 function dimTest(matA, matB) {
     return size(matA)[0] == size(matB)[0] && size(matA)[1] == size(matB)[1] ? true : false
 };
 
-export { numbTest, dimTest };
+export { isNum, dimTest };
