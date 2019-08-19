920908109f913403d2bcfbea2aeaeed9b97ae8b4
Benjamin Heil <bheil@mail.de>
2019-08-19 22:43:06 +0200

    imports removed

--- js/bernoulli.js
+++ js/bernoulli.js
@@ -1,21 +1,18 @@
-import { dimTest, isNum } from 'tests'
-import { transpose, concat, pow } from 'mathjs'
-
 /**
  * Bernoulli distribution 
- * @param   {matrix}    p   p in (0, 1)
- * @param   {matrix}    a   a = 0,1
- * @return  {matrix}
+ * @param   {number|matrix}    p   p in (0, 1)
+ * @param   {number|matrix}    a   a = 0,1
+ * @return  {number|matrix}
  */
-export default function bernoulli(p, a) {
+function bernoulli(p, a) {
     if (isNum(p) && isNum(a)) {
         console.assert(p > 0 && p < 1, "p out of bounds!");
         console.assert(a == 0 || a == 1, "Wrong action!");
-        return pow(p, a) * pow(1 - p, 1 - a);
+        return math.pow(p, a) * math.pow(1 - p, 1 - a);
     }
     else {
         console.assert(dimTest(p, a), "Wrong dimensions!");
-        let pa = transpose(concat(p, a, 0));
+        let pa = math.transpose(math.concat(p, a, 0));
         return pa.map(items => bernoulli(items[0], items[1]));
     }
 };
