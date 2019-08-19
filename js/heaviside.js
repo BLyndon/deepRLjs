ee29496ec2a91401991a180facaa1c78066dbe76
Benjamin Heil <bheil@mail.de>
2019-08-19 22:51:16 +0200

    import/export removed

--- js/heaviside.js
+++ js/heaviside.js
@@ -1,14 +1,12 @@
-import { numbTest } from tests.js
-
 /**
  * Heaviside step func. - applied element-wise:  
  *      x -> 0 if x < 0 
  *      x -> 1 else  
- * @param   {matrix}
- * @return  {matrix}
+ * @param   {number|matrix}
+ * @return  {number|matrix}
  */
-export default function heaviside(x) {
-    if (numbTest(x)) {
+function heaviside(x) {
+    if (isNum(x)) {
         return +(x >= 0);
     }
     else {
