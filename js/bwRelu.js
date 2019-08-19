71530e605ffcc1a3ae90cac455274cdcdb5808a2
Benjamin Heil <bheil@mail.de>
2019-08-19 22:58:02 +0200

    import/export removed

--- js/bwRelu.js
+++ js/bwRelu.js
@@ -1,15 +1,13 @@
-import { dotMultiply, size, } from 'mathjs';
-import heaviside from 'heaviside';
 /**
  * backprop through ReLU nonlinearity:
  * @param   {matrix}    dA
  * @param   {matrix}    cache
  * @return  {matrix}
  */
-export default function bwRelu(dA, cache) {
+function bwRelu(dA, cache) {
     let Z = cache;
-    let dZ = dotMultiply(dA, heaviside(Z));
+    let dZ = math.dotMultiply(dA, heaviside(Z));
 
-    console.assert(dimTest(dZ, Z), "Error bwRelu: shape dZ: " + size(dZ) + ", shape Z: " + size(Z) + ".");
+    console.assert(dimTest(dZ, Z), "Error bwRelu: shape dZ: " + math.size(dZ) + ", shape Z: " + math.size(Z) + ".");
     return dZ;
 }
